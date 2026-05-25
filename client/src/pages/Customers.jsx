213a1b0a9a6d694a5abbea1b709e406fdc195a34
Deepak Prajapat <deepprajapat769@gmail.com>
2026-05-25 17:45:13 +0530

    Frontend -feature--01

---
+++ client/src/pages/Customers.jsx
@@ -0,0 +1,13 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+const Customers = () => {
+  return (
+    <DashboardLayout>
+      <h2 className="text-2xl font-bold mb-5">
+        Customers
+      </h2>
+    </DashboardLayout>
+  );
+};
+
+export default Customers;
---
+++ client/src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+const Products = () => {
+  return (
+    <DashboardLayout>
+      <h2 className="text-2xl font-bold mb-5">
+        Products
+      </h2>
+    </DashboardLayout>
+  );
+};
+
+export default Products;
